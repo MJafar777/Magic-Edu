e168a4baa8bf68d16e08f7b8aff35f45b4a8417a
MagicSoft <magicsoft.uz@gmail.com>
2022-06-21 10:18:40 +0500

    Save file complete section course-section

--- src/components/Courses-section/Courses.jsx
+++ src/components/Courses-section/Courses.jsx
@@ -7,134 +7,133 @@ import "./courses.css";
 import CourseCard from "./CourseCard";
 
 const coursesData = [
-   {
-      id: "01",
-      title: "Frontend (Beginner to MASTER)",
-      lesson: 140,
-      students: 40,
-      rating: 4.9,
-      duration: 7,
-      price: 700_000,
-      imgUrl: courseImg1,
-      subjects: [
-         "HTML",
-         "CSS",
-         "JavaScript",
-         "JQuery",
-         "Github",
-         "Docker",
-         "Firebase",
-         "AWS server",
-         "NextJs",
-         "GraphQL",
-         "REST API",
-         "ReactJs",
-         "Redux",
-         "MobX",
-         "Tailwind CSS",
-         "Material UI",
-         "TypeScript",
-         "Junit",
-         "JWT",
-      ],
-   },
+  {
+    id: "01",
+    title: "Frontend",
+    lesson: 140,
+    students: 40,
+    rating: 4.9,
+    duration: 7,
+    price: 700_000,
+    imgUrl: courseImg1,
+    subjects: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "JQuery",
+      "Github",
+      "Docker",
+      "Firebase",
+      "AWS server",
+      "NextJs",
+      "GraphQL",
+      "REST API",
+      "ReactJs",
+      "Redux",
+      "MobX",
+      "Tailwind CSS",
+      "Material UI",
+      "TypeScript",
+      "Junit",
+      "JWT",
+    ],
+  },
 
-   {
-      id: "02",
-      title: "Backend (Beginner to MASTER)",
-      lesson: 160,
-      students: 55,
-      rating: 5,
-      duration: 8,
-      price: 700_000,
-      imgUrl: courseImg2,
-      subjects: [
-         "HTML",
-         "CSS",
-         "JavaScript Advanced",
-         "Github",
-         "Docker",
-         "GraphQL",
-         "REST API",
-         "NodeJs",
-         "ExpressJs",
-         "Firebase",
-         "JWT",
-         "AWS server",
-         "NestJs",
-         "MongoDB",
-         "Junit",
-         "PostgreSQL",
-         "TypeScript",
-         "Kubernetes",
-         "Algorithms",
-         "Microservice",
-      ],
-   },
+  {
+    id: "02",
+    title: "Backend",
+    lesson: 160,
+    students: 55,
+    rating: 5,
+    duration: 8,
+    price: 700_000,
+    imgUrl: courseImg2,
+    subjects: [
+      "HTML",
+      "CSS",
+      "JavaScript Advanced",
+      "Github",
+      "Docker",
+      "GraphQL",
+      "REST API",
+      "NodeJs",
+      "ExpressJs",
+      "Firebase",
+      "JWT",
+      "AWS server",
+      "NestJs",
+      "MongoDB",
+      "Junit",
+      "PostgreSQL",
+      "TypeScript",
+      "Kubernetes",
+      "Algorithms",
+      "Microservice",
+    ],
+  },
 
-   {
-      id: "03",
-      title: "Mobile App (Beginner to MASTER)",
-      lesson: 140,
-      students: 34,
-      rating: 4.7,
-      duration: 7,
-      price: 700_000,
-      imgUrl: courseImg3,
-      subjects: [
-         "HTML",
-         "CSS",
-         "JavaScript",
-         "Figma",
-         "Github",
-         "Docker",
-         "JWT",
-         "React Native",
-         "ExpressJs",
-         "NodeJs",
-         "Firebase",
-         "REST API",
-         "Redux",
-         "Flow",
-         "TypeScript",
-         "Formik",
-      ],
-   },
+  {
+    id: "03",
+    title: "Mobile App",
+    lesson: 140,
+    students: 34,
+    rating: 4.7,
+    duration: 7,
+    price: 700_000,
+    imgUrl: courseImg3,
+    subjects: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Figma",
+      "Github",
+      "Docker",
+      "JWT",
+      "React Native",
+      "ExpressJs",
+      "NodeJs",
+      "Firebase",
+      "REST API",
+      "Redux",
+      "Flow",
+      "TypeScript",
+      "Formik",
+    ],
+  },
 ];
 
 const Courses = () => {
-   return (
-      <section>
-         <Container id="courses">
-            <Row>
-               <Col lg="12" className="mb-5">
-                  <div className="course__top d-flex justify-content-between align-items-center">
-                     <div className="course__top__left w-50">
-                        <h2>TRENDING COURSES</h2>
-                        <p>
-                           Join latest trending courses in Web Design and
-                           Development at Web Development Institute. All the
-                           courses help students to learn Design and Development
-                           concepts of website!
-                        </p>
-                     </div>
+  return (
+    <section>
+      <Container id="courses">
+        <Row>
+          <Col lg="12" className="mb-5">
+            <div className="course__top d-flex justify-content-between align-items-center">
+              <div className="course__top__left w-50">
+                <h2>TRENDING COURSES</h2>
+                <p>
+                  Join latest trending courses in Web Design and Development at
+                  Web Development Institute. All the courses help students to
+                  learn Design and Development concepts of website!
+                </p>
+              </div>
 
-                     <div className="w-50 text-end">
-                        <button onClick={console.log("ss")} className="btn">
-                           See All
-                        </button>
-                     </div>
-                  </div>
-               </Col>
-               {coursesData.map((item, key) => (
-                  <Col key={key} lg="4" md="6" sm="6">
-                     <CourseCard key={item.id} item={item} />
-                  </Col>
-               ))}
-            </Row>
-         </Container>
-      </section>
-   );
+              <div className="w-50 text-end">
+                <button onClick={console.log("ss")} className="btn">
+                  See All
+                </button>
+              </div>
+            </div>
+          </Col>
+          {coursesData.map((item, key) => (
+            <Col key={key} lg="4" md="6" sm="6">
+              <CourseCard key={item.id} item={item} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Courses;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,71 +3,90 @@ import { Container } from "reactstrap";
 import "./header.css";
 
 const navLinks = [
-   {
-      display: "Home",
-      url: "#home",
-   },
-   {
-      display: "About Us",
-      url: "#about",
-   },
+  {
+    display: "Home",
+    url: "#home",
+  },
+  {
+    display: "About Us",
+    url: "#about",
+  },
 
-   {
-      display: "Courses",
-      url: "#courses",
-   },
-   //  {
-   //     display: "Pages",
-   //     url: "#",
-   //  },
-   {
-      display: "Blog",
-      url: "#",
-   },
+  {
+    display: "Courses",
+    url: "#courses",
+  },
+  //  {
+  //     display: "Pages",
+  //     url: "#",
+  //  },
+  {
+    display: "Blog",
+    url: "#footer",
+  },
 ];
 
 const Header = () => {
-   const menuRef = useRef();
+  const menuRef = useRef();
 
-   const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
 
-   return (
-      <header className="header">
-         <Container>
-            <div className="navigation d-flex align-items-center justify-content-between">
-               <div className="logo">
-                  <h2 className=" d-flex align-items-center gap-1">
-                     <i className="ri-pantone-line"></i> MagicSoft
-                  </h2>
-               </div>
+  return (
+    <header className="header">
+      <Container>
+        <div className="navigation d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <h2 className=" d-flex align-items-center gap-1">
+              <i className="ri-pantone-line"></i> MagicSoft
+            </h2>
+          </div>
 
-               <div className="nav d-flex align-items-center gap-5">
-                  <div className="nav__menu" ref={menuRef} onClick={menuToggle}>
-                     <ul className="nav__list">
-                        {navLinks.map((item, index) => (
-                           <li key={index} className="nav__item">
-                              <a href={item.url}>{item.display}</a>
-                           </li>
-                        ))}
-                     </ul>
+          <div className="nav d-flex align-items-center gap-5">
+            <div className="nav__menu" ref={menuRef} onClick={menuToggle}>
+              <ul className="nav__list">
+                {navLinks.map((item, index) => (
+                  <li key={index} className="nav__item">
+                    <a href={item.url}>{item.display}</a>
+                  </li>
+                ))}
+                <li>
+                  <div className="nav__item">
+                    <a href="https://reg.magicsoft.uz/" target="__blank">
+                      <button className="btn">Register Course</button>
+                    </a>
                   </div>
-
-                  <div className="nav__right">
-                     <p className="mb-0 d-flex align-items-center gap-2">
-                        <i className="ri-phone-line"></i> +99894 651-81-41
-                     </p>
+                </li>
+                <li>
+                  <div className="nav__item">
+                    <p className="mt-4 d-flex align-items-center">
+                      <i className="ri-phone-line"></i> +99894 651-81-41
+                    </p>
                   </div>
-               </div>
-
-               <div className="mobile__menu">
-                  <span>
-                     <i className="ri-menu-line" onClick={menuToggle}></i>
-                  </span>
-               </div>
+                </li>
+              </ul>
             </div>
-         </Container>
-      </header>
-   );
+
+            {/* <div className="nav__right">
+              <p className="mb-0 d-flex align-items-center gap-2">
+                <i className="ri-phone-line"></i> +99894 651-81-41
+              </p>
+            </div>
+            <div className="nav__right">
+              <a href="https://reg.magicsoft.uz/" target="__blank">
+                <button className="btn">Register Course</button>
+              </a>
+            </div> */}
+          </div>
+
+          <div className="mobile__menu">
+            <span>
+              <i className="ri-menu-line" onClick={menuToggle}></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
